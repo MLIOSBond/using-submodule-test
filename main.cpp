459edd462056eca7c39d1c8a43699412f6040d83
MLIOSBond <bond31337@gmail.com>
2020-01-03 22:33:27 +0300

    example of using a submodule

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "submodule-test/somelib.h"
 
 int main()
 {
     std::cout << "Hi there!" << "\n";
+    std::cout << "Squared value (5): " << squared(5) << "\n";
     return 0;
 }
