5bd0ae5949a11f79df066738d95a07ba93096a81
MLIOSBond <bond31337@gmail.com>
2020-01-06 16:32:50 +0300

    add namespace somelib

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@
 int main()
 {
     std::cout << "Hi there!" << "\n";
-    std::cout << "Squared value (5): " << squared(5) << "\n";
+    std::cout << "Squared value (5): " << somelib::squared(5) << "\n";
     return 0;
 }
